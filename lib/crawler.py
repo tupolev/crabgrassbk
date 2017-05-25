4acc7bd499d8733d87dd0167637e14757a287f76
tupolev <tupolev@gmail.com>
2017-05-25 21:53:34 +0200

    -covered urllib exception on retrieving link

--- lib/crawler.py
+++ lib/crawler.py
@@ -71,7 +71,7 @@ class Crawler:
             self.driver.wait.until(EC.presence_of_all_elements_located)
             self.dump_page(output_dir)
         except URLError as ex:
-            print(ex.reason)
+            print(link, ex.reason)
 
     def dump_page(self, output_dir: str):
         title_start = re.search(r"<title>", self.driver.page_source[1:1500])
